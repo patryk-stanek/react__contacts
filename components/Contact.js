864ac395ed020c10604fe5168e6911a9fc075038
Patryk Stanek <p.stanek3@gmail.com>
2019-04-01 18:18:41 +0200

    JSX

--- components/Contact.js
+++ components/Contact.js
@@ -6,14 +6,19 @@ var Contact = React.createClass({
     render: function() {
         return (
             <div className={'contactItem'}>
-                <img className={'contactImage'} src={'link-do-obrazka.png'}/>
-                <p className={'contactLabel'}>
-                    Imię: {this.props.contact.firstName}
-                </p>
-                <a href={'mailto:' + this.props.item.email}>
-                    {this.props.item.email}
-                </a>
+                <img className={'contactImage'} src={'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png'}/>
+                <div className={'contactInfo'}>
+                    <p className={'contactLabel'}>
+                        Imię: {this.props.item.firstName}
+                    </p>
+                    <p className={'contactLabel'}>
+                        Nazwisko: {this.props.item.lastName}
+                    </p>
+                    <a href={'mailto:' + this.props.item.email}>
+                        {this.props.item.email}
+                    </a>
+                </div>
             </div>
         )
     }
-})
+});
